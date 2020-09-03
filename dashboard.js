f2cee8dfb80013b8d51f3be0b6021c7fdc168353
professorisidro <isidro@professorisidro.com.br>
2020-09-03 11:57:04 -0300

    gerando relatório

---
+++ dashboard.js
@@ -0,0 +1,57 @@
+function carregaInfo(){
+    var infoUser = localStorage.getItem("pdvUser");
+    if (!infoUser){
+        window.location = "index.html";
+    }
+    var objUser  = JSON.parse(infoUser);
+
+    var linhaHTML = `<div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                          <img src="${objUser.linkFoto}" width="100%">
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+                           <strong> Nome: </strong> ${objUser.nome} <br>
+                           <strong> RACF: </strong> ${objUser.racf} <br>
+                           <strong> Email: </strong> ${objUser.email} <br>
+                           <strong> Fone: </strong> ${objUser.telefone} <br>
+                        </div>
+                     </div>`;
+    document.getElementById("dadosUsuario").innerHTML = linhaHTML;
+
+    recuperaRelatorio();
+}
+
+
+function recuperaRelatorio(){
+    fetch("http://localhost:8088/solicitacao/todas")
+      .then(res=>res.json())
+      .then(lista=>preencheRelatorio(lista));
+}
+
+function preencheRelatorio(lista){
+    var relHTML = "";
+    for (i=0;i<lista.length; i++){
+        var solic=lista[i];
+
+        relHTML = relHTML+`<div class="row">
+                             <div class="col-1"> ${solic.numSeq} </div>
+                             <div class="col-2"> ${solic.dataSolicitacao} ${solic.horaSolicitacao} </div>
+                             <div class="col-4"> ${solic.nomeTecnico} <br>
+                                                 ${solic.documento} / ${solic.telefone} </div>
+                             <div class="col-3"> ${solic.pdv.nome} </div>
+                             <div class="col-2"> <button type="button" class="btn btn-success" 
+                                                         onclick="atualizarStatus(${solic.numSeq},1)"> &nbsp; </button>
+                                                 <button type="button" class="btn btn-danger" 
+                                                         onclick="atualizarStatus(${solic.numSeq},2)"> &nbsp; </button>
+                                                 <button type="button" class="btn btn-secondary" 
+                                                         onclick="atualizarStatus(${solic.numSeq},3)"> &nbsp; </button>
+                             </div>
+                          </div>`;
+    }
+    document.getElementById("relatorio").innerHTML = relHTML;
+}
+
+
+function atualizarStatus(numReq, novoStatus){
+    alert("Alterando requisicao #"+numReq+" para estado "+novoStatus);
+}
